732ff9abc7aea0c48da9980946ed475f5aee1a3c
Dark Lord Technologist <119113939+lord-Vampirion-Tech@users.noreply.github.com>
2025-08-21 18:43:04 +0300

    Update ferch_vk.js

--- ferch_vk.js
+++ ferch_vk.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
 
+const TOKEN = process.env.VK_TOKEN; // токен берём из GitHub Secrets
+const ALBUM_ID = '164068548';       // ID альбома (из ссылки)
+const OWNER_ID = '-44163951';      // ID группы (ставим минус перед ID сообщества)
+
+async function fetchPhotos() {
+  const url = https://api.vk.com/method/photos.get?owner_id=${OWNER_ID}&album_id=${ALBUM_ID}&access_token=${TOKEN}&v=5.131;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!data.response) {
+    console.error('Ошибка:', data);
+    return;
+  }
+
+  const photos = data.response.items.map(photo => {
+    const sizes = photo.sizes;
+    const maxSize = sizes[sizes.length - 1]; // берём самую большую картинку
+    return maxSize.url;
+  });
+
+  fs.writeFileSync('photos.json', JSON.stringify(photos, null, 2));
+  console.log('Фотографии сохранены в photos.json');
+}
+
+fetchPhotos();
